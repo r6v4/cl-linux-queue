f2ab3dc1202b0e756c0bf5c55019f31d290e1ceb
r6v4 <r6v4@pm.me>
2024-08-30 17:18:15 +0800

    Update user-function.lisp

--- code/user-function.lisp
+++ code/user-function.lisp
@@ -1,10 +1,10 @@
 (in-package :cl-linux-queue)
 
-(defun make-mailbox (id)
-    (msgget id 950)) ;
+(defun make-mailbox (&optional (id 12345))
+    (msgget id 950) ) ;
 
 (defun send-message (queue message)
-    (send-message-macro queue message))
+    (send-message-macro queue message) )
 
 (defun receive-message (queue pointer)
-    (receive-message-macro queue pointer))
+    (receive-message-macro queue pointer) )
